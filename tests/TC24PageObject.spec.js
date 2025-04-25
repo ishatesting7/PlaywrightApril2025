599210edb6501f0dce92f0bbe22b0e0d29448e41
harafit <harafittesting@gmail.com>
2025-04-25 22:00:55 +0530

    Added

--- tests/TC24PageObject.spec.js
+++ tests/TC24PageObject.spec.js
@@ -1,20 +1,37 @@
 import {expect, test} from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { HomePage } from "../pages/HomePage";
+let lp;
+let hp;
 
+test.beforeEach('Description for Before Each', async({page})=>{
+
+    lp = new LoginPage(page);
+    hp = new HomePage(page);
+
+})
 
 test('Navigate to Login Page', async({page})=>{
 
 
-    const lp = new LoginPage(page);
 
     await lp.navigateToLogin();
     await lp.enterLoginCreds("demo","demo");
     await page.waitForTimeout(3000);
-
-    const hp = new HomePage(page);
     await hp.validateLogoutBtn();
     
+    
+
+})
+
+test('Click Close Button', async({page})=>{
+
+
+
+    await lp.navigateToLogin();
+    await page.waitForTimeout(3000);
+
+    await lp.closeLoginPopup();   
 
 
 })

abf0ed400dafb514d2ee797d1a9a52a0477716b6
harafit <harafittesting@gmail.com>
2025-04-28 21:32:01 +0530

    Added

--- tests/saucedemo/cart.test.spec.js
+++ tests/saucedemo/cart.test.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test');
 const LoginPage = require('../../pages/saucedemo/loginpage');
 const InventoryPage = require('../../pages/saucedemo/inventorypage');
 const CartPage = require('../../pages/saucedemo/cartpage');
+const common = require('../../pages/utility/common')
 
 test.describe('Cart Tests', () => {
   let loginPage;
@@ -13,12 +14,35 @@ test.describe('Cart Tests', () => {
     loginPage = new LoginPage(page);
     inventoryPage = new InventoryPage(page);
     cartPage = new CartPage(page);
+    common = new common(page);
+
     await loginPage.navigate();
     await loginPage.login('standard_user', 'secret_sauce');
   });
 
   test('should navigate to cart', async () => {
     await inventoryPage.goToCart();
-    await expect(cartPage.checkoutButton).toBeVisible();
+    //await expect(cartPage.checkoutButton).toBeVisible();
+    await common.validateButton(cartPage.checkoutButton);
   });
 });
+
+/*
+
+button is visible
+text is present
+alert message
+toast message
+
+utility.js
+async validateButton(locator, text)
+{
+  expect(locator).toBeVisible();
+}
+
+async validateTodaysDate(locator, today)
+{
+  expect(locator)
+}
+
+*/
